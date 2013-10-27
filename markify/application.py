ce492bff9f39ae227808490148f4fddb05aad899
dhsheng <distreet.com@gmail.com>
2013-10-27 21:33:13 +0800

    url map for product, customer, order

--- markify/application.py
+++ markify/application.py
@@ -9,21 +9,33 @@ import config
 from markify.handlers import user
 from markify.handlers import order
 from markify.handlers import product
+from markify.handlers import customer
 
 
 class Application(BaseApplication):
 
     def __init__(self):
         handlers = [
+
             url(r'/order/create', order.CreateRequestHandler, name='order.create'),
+            url(r'/order/edit', order.EditRequestHandler, name='order.edit'),
+            url(r'^/$', order.ListRequestHandler, name='orders'),
             url(r'/product/create', product.CreateRequestHandler, name='product.create'),
             url(r'/products', product.ListRequestHandler, name='products'),
+            url(r'/product/edit', product.EditRequestHandler, name='product.edit'),
+            url(r'/product/delete', product.DeleteRequestHandler, name='product.delete'),
             url(config.LOGIN_URL, user.LoginRequestHandler, name='login'),
+            url(r'/customers', customer.ListRequestHandler, name='customers'),
+            url(r'/customer/create', customer.CreateRequestHandler, name='customer.create'),
+            url(r'/customer/edit', customer.EditRequestHandler, name='customer.edit'),
+            url(r'/customer/delete', customer.DeleteRequestHandler, name='customer.delete'),
+            url(r'/customer/view', customer.ViewRequestHandler, name='customer.view'),
             url(r'/register', user.RegisterRequestHandler, name='register'),
             url(r'/logout', user.LogoutRequestHandler, name='logout'),
         ]
+
         settings = {
-            'Debug': config.DEBUG,
+            'debug': config.DEBUG,
             'cookie_secret': config.SESSION_KEY,
             'template_path': config.TEMPLATE_DIRS,
             'login_url': config.LOGIN_URL,
